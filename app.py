298c3d8c6021e199b92f0789e6fb6667120329e1
adiamantaras-ux <adiamantaras@gmail.com>
2025-11-26 13:21:04 +0200

    Update app.run configuration for production
    Change app.run to use environment port and disable debug mode.

--- app.py
+++ app.py
@@ -133,4 +133,6 @@ def download(event_name):
                      download_name=f'{event_name}_κρατήσεις.csv')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
